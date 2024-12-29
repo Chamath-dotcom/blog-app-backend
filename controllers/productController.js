a59a7cd16e5edfa0380e2474a84e29d2cc08a1a3
Chamath Kalhara <ckalhara360@gmail.com>
2024-12-30 00:35:45 +0530

    add some restrictions to addProduct✅(NO BUGS🔥)

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,6 +2,18 @@ import Product from "../models/product.js";
 
 
 export default  function addProduct(req,res){
+    console.log(req.user);
+    if(req.user == null)
+    {
+    res.status(401).json({message:"please login and try agin!"});
+    return
+    }
+    if(req.user.role !=="admin")
+    {
+    res.status(403).json({message : "You are not an Admin"})
+    return
+    }
+
     const productData = req.body;
     const newProduct = new Product(productData)
     newProduct.save()
