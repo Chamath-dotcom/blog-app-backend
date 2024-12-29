39348ecc6dd208bd75a7ae4ed8571a35aaaf5a4f
Chamath Kalhara <ckalhara360@gmail.com>
2024-12-29 22:15:36 +0530

    create mongoDB connection✅

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = 5000
@@ -7,3 +7,10 @@ app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
+const mongoUrl = "mongodb+srv://admin:123@cluster0.f1hjr.mongodb.net/kv_audio?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(mongoUrl);
+const connection =mongoose.connection;
+connection.once('open',()=>{
+    console.log("connect to mongo DB");
+    
+})
