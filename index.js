1d5af1a8e4c21a5d48f294609084a2dedc9ece84
Chamath Kalhara <ckalhara360@gmail.com>
2025-01-09 19:57:04 +0530

    app crashed 🚫

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import userRoute from './routers/userRoute.js';
 import jwt from 'jsonwebtoken'
 import productRoute from './routers/productRoute.js';
 import dotenv from 'dotenv'
+import reviewRouter from "./routes/reviewRouter.js";
 
 dotenv.config();
 
@@ -47,3 +48,4 @@ app.use((req,res,next)=>{
 
 app.use("/api/users",userRoute);
 app.use("/api/product",productRoute);
+app.use("/api/reviews", reviewRouter);
