65b4b82216f1ca2663fdf14f092d368edf1631fa
Chamath Kalhara <ckalhara360@gmail.com>
2025-01-17 03:23:52 +0530

    create get product async function✅

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,29 +1,42 @@
 import Product from "../models/product.js";
 
 
-export function addProduct(req,res){
+export async function addProduct(req,res){
     console.log(req.user);
-    if(req.user == null)
-    {
-    res.status(401).json({message:"please login and try agin!"});
-    return
-    }
-    if(req.user.role !=="admin")
-    {
-    res.status(403).json({message : "You are not an Admin"})
-    return
-    }
-
+    const user = req.user;
     const productData = req.body;
     const newProduct = new Product(productData)
-    newProduct.save()
-    .then(
-        ()=>{
-            res.json({message :`${productData.productName} added !`})
+    try{
+        if(user == null || user.role !=="admin")
+        {
+            res.status(401).json({message:"you can't access this"});
+            return
         }
-    ).catch(
-        ()=>{
-            res.status(500).json({error:`product addition failed !`})
+        if(user.role =="admin"){
+            const saveprod = await newProduct.save();
+            if(saveprod){
+                res.json({message :`${productData.productName} added !`})
+            }else{
+                res.status(500).json({error:`product addition failed !`})
+            }
         }
-    )
+    }
+    catch{
+        res.json({message:"something went wrong!!"});
+    }
+
+}
+
+export async function getProduct(req,res){
+    try{
+        const foundProd = await Product.find();
+        if(foundProd){
+            res.json({message:foundProd})
+        }else{
+            res.json({messag:"can't found product!"})
+        }
+    }catch{
+        res.json({message :"something went wrong!!"})
+    }
+
 }
