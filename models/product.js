d88164ad829d2cbb5b602a4ad850dff54b5b5cce
Chamath Kalhara <ckalhara360@gmail.com>
2025-01-20 18:43:30 +0530

    modify the productModel ✅

--- models/product.js
+++ models/product.js
@@ -1,18 +1,42 @@
 import mongoose from 'mongoose';
 
 const productSchema =new mongoose.Schema({
-    productName :{
-        type:String,
+    key :{
+        type : String,
+        required : true,
+        unique : true
+      },
+      name : {
+        type : String,
         required : true
-    },
-    price :{
-        type:Number,
+      },
+      price : {
+        type : Number,
         required : true
-    },
-    dectription :{
-        type:String,
+      },
+      category : {
+        type : String,
+        required : true,
+        default : "uncategorized"
+      },
+      dimensions : {
+        type : String,
         required : true
-    }
+      },
+      description : {
+        type : String,
+        required : true
+      },
+      availability : {
+        type : Boolean,
+        required : true,
+        default : true
+      },
+      image:{
+        type : [String],
+        required : true,
+        default : ["https://www.shutterstock.com/image-vector/user-profile-icon-vector-avatar-600nw-2247726673.jpg"]
+      }
 })
 
 const Product =mongoose.model('product',productSchema);
